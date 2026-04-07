2d0f7b1b68e93b117776c579727864920be59ab3
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-08 01:05:22 +0300

    chore: update example.py header with task description and author information

--- example.py
+++ example.py
@@ -1,12 +1,11 @@
 """
-COMP.CS.100 Programming 1: Introduction to Programming implementation for
-2026 spring
+COMP.CS.100 Programming 1.
+Jingjing Yang, jingjing.yang@tuni.fi, student id 154016843.
+Solution of task 1.6.4: A program that prints a cheerful greeting to the user.
 
 Learning Goals:
-
-Creator: Jingjing Yang
-Student id number: 154016843
 """
+
 def main():
     print("")
 
