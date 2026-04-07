8bef8a6304e9a22d9b99facfa9b00151b37835bb
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-08 00:48:40 +0300

    chore: add example.py with main function as a template

---
+++ example.py
@@ -0,0 +1,14 @@
+"""
+COMP.CS.100 Programming 1: Introduction to Programming implementation for
+2026 spring
+
+Learning Goals:
+
+Creator: Jingjing Yang
+Student id number: 154016843
+"""
+def main():
+    print("")
+
+if __name__ == "__main__":
+    main()
