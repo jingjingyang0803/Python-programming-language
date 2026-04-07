88c551cb2866e06c5a0313d242cb863862021a53
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-08 00:07:47 +0300

    exercise1.6.6: implement main function for greeting program

---
+++ week01/main_tervehdys.py
@@ -0,0 +1,20 @@
+"""
+COMP.CS.100 Programming 1: Introduction to Programming implementation for
+2026 spring
+
+Learning Goals:
+Learning to write a main function to a program.
+
+Creator: Jingjing Yang
+Student id number: 154016843
+"""
+def main():
+    name = input("Tell your name: ")
+    print("Hi " + name)
+    print("your coding skills are great!")
+
+    # If the program had some other tasks to perform
+    # the commands for them would be written here.
+
+if __name__ == "__main__":
+    main()
