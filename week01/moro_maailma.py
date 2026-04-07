023a745680310cb1355ff719a93aa6724f70e5a7
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-07 23:29:07 +0300

    exercise1.5.1: write first python program - hello world

---
+++ week01/moro_maailma.py
@@ -0,0 +1,9 @@
+"""
+COMP.CS.100 Programming 1: Introduction to Programming implementation for
+2026 spring
+The first Python program.
+Creator: Jingjing Yang
+Student id number: 154016843
+"""
+
+print("Hello World!")
