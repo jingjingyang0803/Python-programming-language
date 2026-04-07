1a10a84fc1a9051ee57c9c25c3289dcfb1488aca
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-07 23:49:31 +0300

    exercise1.6.3: write greeting program to practice reading user input and variable

---
+++ week01/tervehdys.py
@@ -0,0 +1,18 @@
+"""
+COMP.CS.100 Programming 1: Introduction to Programming implementation for
+2026 spring
+Learning Goals:
+Familiarizing yourself with variables and reading the user's input.
+
+Create a program that asks the name of the user and then greets them with
+the text in the example run below.
+Tell your name: Teemu
+Hi Teemu
+your coding skills are great!
+
+Creator: Jingjing Yang
+Student id number: 154016843
+"""
+name = input("Tell your name: ")
+print("Hi " + name)
+print("your coding skills are great!")
