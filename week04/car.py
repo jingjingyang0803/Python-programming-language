95ac46a98b8d86e235cb98e6eff3457f23e0a0d1
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-10 14:43:30 +0300

    exercise4.14: implement car driving simulation with gas management and distance calculation

--- week04/car.py
+++ week04/car.py
@@ -1,6 +1,15 @@
 """
-COMP.CS.100 Programming 1 code template
-Fill in all TODOs in this file
+COMP.CS.100 Programming 1.
+Jingjing Yang, jingjing.yang@tuni.fi, student id 154016843.
+Solution of task 4.14 - Car:
+ In this task, you shall create a simple driving simulation where the user
+  drives a car through a two-dimensional desert surface, using a textual
+  interface. The program calculates where the car moves and how much gas it
+  uses.
+
+
+Learning Goals:
+ Learning to implement functions that contain calculations.
 """
 
 from math import sqrt
@@ -20,20 +29,22 @@ def menu():
     x = 0.0  # Current X coordinate of the car
     y = 0.0  # Current Y coordinate of the car
 
-    while True:
-        print("Coordinates x={:.1f}, y={:.1f}, "
-              "the tank contains {:.1f} liters of gas.".format(x, y, gas))
+    print_result(x, y, gas)
 
+    while True:
         choice = input("1) Fill 2) Drive 3) Quit\n-> ")
 
         if choice == "1":
             to_fill = read_number("How many liters of gas to fill up? ")
-            TODO = fill(TODO)
+            gas = fill(tank_size, to_fill, current_gas=gas)
+            print_result(x, y, gas)
 
         elif choice == "2":
             new_x = read_number("x: ")
             new_y = read_number("y: ")
-            TODO = drive(TODO)
+            gas, x, y = drive(x, y, new_x, new_y, gas,
+                                       gas_consumption)
+            print_result(x, y, gas)
 
         elif choice == "3":
             break
@@ -41,7 +52,7 @@ def menu():
     print("Thank you and bye!")
 
 
-def fill(TODO):
+def fill(tank_size,fill_gas, current_gas):
     """
     This function has three parameters which are all FLOATs:
       (1) the size of the tank
@@ -56,10 +67,13 @@ def fill(TODO):
     input.
     """
 
-    TODO
+    if tank_size - current_gas >= fill_gas:
+        return current_gas + fill_gas
+    else:
+        return tank_size
 
 
-def drive(TODO):
+def drive(current_x, current_y, dest_x, dest_y, current_gas, consumption_per_km):
     """
     This function has six parameters. They are all floats.
       (1) The current x coordinate
@@ -83,13 +97,52 @@ def drive(TODO):
     # It might be usefull to make one or two assisting functions
     # to help the implementation of this function.
 
-    TODO
+    km_to_drive=sqrt((dest_x-current_x)**2+(dest_y-current_y)**2)
+    gas_needed=km_to_drive*consumption_per_km/100
+    if gas_needed > current_gas:
+        km_drivable=current_gas*100/consumption_per_km
+        ratio=km_drivable/km_to_drive
+        new_x, new_y=calculate_distance(current_x,current_y,dest_x,dest_y,
+                                         ratio)
+        return 0.0, new_x, new_y
+    else:
+        return current_gas-gas_needed, dest_x, dest_y
 
 
-TODO  # Implement your own functions here. You are required to
+      # Implement your own functions here. You are required to
       # implement at least two functions that take at least
       # one parameter and return at least one value.  The
       # functions have to be used somewhere in the program.
+
+def calculate_distance(x1, y1, x2, y2, ratio):
+    """
+    Calculate a point between two coordinates based on a given ratio.
+
+    The function returns a point that divides the line segment between
+    (x1, y1) and (x2, y2) according to the specified ratio.
+
+    :param x1: x-coordinate of the first point
+    :param y1: y-coordinate of the first point
+    :param x2: x-coordinate of the second point
+    :param y2: y-coordinate of the second point
+    :param ratio: A float representing how far the point is from the first point
+                  toward the second point.
+                  (0 returns the first point, 1 returns the second point)
+    :return: A tuple (x, y) representing the calculated point
+    """
+    new_x = x1 + (x2 - x1) * ratio
+    new_y = y1 + (y2 - y1) * ratio
+    return new_x, new_y
+
+def print_result(x,y,gas):
+    """This function prints out the current coordinates of the car and the amount of gas in the tank.
+
+    :param x: x-coordinate of the car
+    :param y: y-coordinate of the car
+    :param gas: amount of gas in the tank
+    """
+    print("Coordinates x={:.1f}, y={:.1f}, the tank contains {:.1f} "
+          "liters of gas.".format(x, y, gas))
 
 
 def read_number(prompt, error_message="Incorrect input!"):
