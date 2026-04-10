9f038c01558c0d9a33ba2fa0dcbcc962e12705e0
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-10 21:41:08 +0300

    project6.2: implement program to print energy consumption histograms

--- week06/energy_statistic_project.py
+++ week06/energy_statistic_project.py
@@ -5,11 +5,35 @@ Email:      jingjing.yang@tuni.fi
 Student Id: 154016843
 
 6.2 - Project: Energy statistic:
- Implement a program capable of printing energy cunsumption histograms based
-  on the data the user entered. The energy consumption values will be
-  divided into classes based on a logarithmic scale. In practise this
-  means that the limits of the classes (or categories) of the values will
-  differ from each other by multiples of 10.
+ A program printing energy cunsumption histograms based on the data the
+  user entered. The energy consumption values will be divided into classes
+  based on a logarithmic scale.
+
+Note: All the inputs for this program are integers (int) for simplicity.
+     The program should be able to handle any non-negative integer values.
+
+    Class number    Values belonging to the class
+    1.              0–9
+    2.              10–99
+    3.              100–999
+    4.              1000–9999
+    5.              10000–99999
+    ...             ...–...
+
+Example output:
+    Enter energy consumption data.
+    End by entering an empty line.
+
+    Enter energy consumption (kWh): 91
+    Enter energy consumption (kWh): 10
+    Enter energy consumption (kWh): 101
+    Enter energy consumption (kWh): 912
+    Enter energy consumption (kWh): 22
+    Enter energy consumption (kWh): 6
+    Enter energy consumption (kWh):
+        0-9: *
+      10-99: ***
+    100-999: **
 
 Learning Goals:
  Practise materials covered in the class in weeks 1–5.
@@ -20,24 +44,127 @@ def main():
     print("End by entering an empty line.")
     print()
 
-    # Test values for the input data, so they don't have to be manually
-    # entered every single time you want to test your program.  Before
-    # submitting your program to Plussa these must be replaced by values
-    # read from the user, otherwise the tests will fail.
-    input_data = [
-        22, 3, 4444, 333, 1000000000, 4, 33, 1, 55,
-        5555, 2, 555, 44, 55555, 444, 999999999, 5,
-    ]
+    data = read_input()
 
-    print_histogram(input_data)
+    # Do not print the histogram if there is no data to print.
+    if not data:
+        print("Nothing to print. Done.")
+        return
+
+    # Print the histogram of the energy consumption values entered by the user.
+    print_histogram(data)
+
+
+def read_input():
+    """
+    Reads energy consumption values from the user until an empty line is
+    entered.  The values are returned as a list of integers.
+
+    :return: list of int
+        The energy consumption values entered by the user.
+    """
+    input_data = []
+
+    while True:
+        user_input = input("Enter energy consumption (kWh): ")
+
+        # If the user input is an empty line, break the loop.
+        if user_input == "":
+            break
+
+        value = int(user_input) # Assume all input are integers for simplicity.
+
+        # If the user input is negative, print an error message and
+        # ask for input again.
+        if value < 0:
+            print(f"You entered: {value}. Enter non-negative numbers only!")
+            continue
+
+        # If the user input is valid, add it to the input data list.
+        input_data.append(value)
+
+    return input_data
+
+
+def class_minimum_value(class_number):
+    """
+    Returns the smallest value of the given class number.
+
+    :param class_number: int
+        The class to find the smallest value.
+
+    :return: int
+        The smallest value of the class with the given <class_number>.
+    """
+    smallest_value = 10 ** class_number // 100 * 10
+    return smallest_value
+
+
+def class_maximum_value(class_number):
+    """
+    Returns the largest value of the given class number.
+
+    :param class_number: int
+        The class to find the largest value.
+
+    :return: int
+        The largest value of the class with the given <class_number>.
+    """
+    largest_value  = 10 ** class_number - 1
+    return largest_value
+
+
+def get_class_number(value):
+    """
+    Returns the class number the given value belongs to.
+
+    :param value: int
+        The value to check
+
+    :return: int
+        The class number the given value belongs to.
+    """
+    class_number = 1
+    while True:
+        if (class_minimum_value(class_number) <= value <=
+                 class_maximum_value(class_number)):
+           return class_number
+        class_number += 1
+
+
+def get_class_counts(data):
+    """
+    Counts how many values in the given data belong to each class.
+
+    :param data: list of int
+        The energy consumption values entered by the user.
+
+    :return: list of int
+        Counts of how many values in the given data belong to each class.
+        The value at index n of the returned list is the count of values
+        belonging to class n.
+    """
+    # Calculate the number of classes needed to cover all the values in the
+    # input data.
+    number_of_categories = get_class_number(max(data))
+
+    # Initialize the class counts list with zeros.
+    # The class numbers start from 1(0-9), so we need to add 1 to the
+    # number of categories to have enough
+    class_counts = [0] * (number_of_categories + 1)
+
+    # For each value in the input data, determine which class it belongs to
+    # and increment the count for that class.
+    for value in data:
+        class_number = get_class_number(value)
+        class_counts[class_number] += 1
+
+    return class_counts
 
 
 def print_single_histogram_line(class_number, count, largest_class_number):
     """
-    This function is probably the most challenging one in this assignment.
-    It will print one correctly indented histogram line as described
-    in the assignment instructions. Your job is to call it with
-    correct parameters.
+    Prints one correctly indented histogram line.
 
     :param class_number: int,
         Expresses which consumption class (1, 2, 3, ...)
@@ -58,8 +185,6 @@ def print_single_histogram_line(class_number, count, largest_class_number):
 
     # <range_string> represents the range of the values the line's
     # histogram will represent in the printout.  For example "1000-9999".
-    # Uses the functions class_minimum_value and class_maximum_value which
-    # have to be defined elsewhere.
 
     min_value = class_minimum_value(class_number)
     max_value = class_maximum_value(class_number)
@@ -82,6 +207,25 @@ def print_single_histogram_line(class_number, count, largest_class_number):
     # (filler white spaces will be printed in the beginning).
 
     print(f"{range_string:>{largest_width}}: {'*' * count}")
+
+
+def print_histogram(data):
+    """
+    Prints the histogram of the energy consumption values in the given data.
+
+    :param data: list of int
+        The energy consumption values entered by the user.
+    """
+    class_counts = get_class_counts(data)
+
+    # The largest class number is the last index in the counts list
+    largest_class_number = len(class_counts) - 1
+
+    # Print the histogram lines for all the classes from 1 to the largest
+    # class number.
+    for class_number in range(1, largest_class_number + 1):
+        count = class_counts[class_number]
+        print_single_histogram_line(class_number, count, largest_class_number)
 
 
 if __name__ == "__main__":
