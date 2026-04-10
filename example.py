0097f342d76cdbb64eb3ed626c20454ab3dc1a80
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-10 17:38:57 +0300

    chore: update example.py

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 """
 COMP.CS.100 Programming 1.
 Jingjing Yang, jingjing.yang@tuni.fi, student id 154016843.
-Solution of task 1.6.4: A program that prints a cheerful greeting to the user.
+Solution of task 8:
+
 
 Learning Goals:
+
 """
 
 def main():
