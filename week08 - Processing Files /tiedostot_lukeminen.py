0eef73e3c7cdbc1e7f5c53365d7b86f8abe6248b
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-12 23:41:10 +0300

    exercise8.6: implement file line numbering program

---
+++ week08 - Processing Files /tiedostot_lukeminen.py
@@ -0,0 +1,25 @@
+"""
+COMP.CS.100 Programming 1.
+Jingjing Yang, jingjing.yang@tuni.fi, student id 154016843.
+Solution of task 8.6 - Numbering the file lines:
+ Create a program that reads a file and prints its contents with each row
+ starts with the row's number and a space.
+
+Learning Goals:
+ I learn the details related to processing files.
+"""
+
+def main():
+    file_name=input("Enter the name of the file: ")
+    try:
+        file=open(file_name, mode="r")
+        i=1
+        for line in file:
+            print(i,line.rstrip())
+            i+=1
+
+    except IOError:
+        print("The file does not exist")
+
+if __name__ == "__main__":
+    main()
