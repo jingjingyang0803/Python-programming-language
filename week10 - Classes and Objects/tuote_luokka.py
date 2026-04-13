8b4ec16159e96293bdda2582f0d843e03d67b192
Jingjing Yang <jingjing.yang@tuni.fi>
2026-04-13 23:30:05 +0300

    exercise10.4: implement Product class with initialization and sale functionality

--- week10 - Classes and Objects/tuote_luokka.py
+++ week10 - Classes and Objects/tuote_luokka.py
@@ -13,27 +13,26 @@ class Product:
     This class defines a simplified product for sale in a store.
     """
 
-    # TODO: Define all the methods here.  You can see what they are,
-    #       what parameters they take, and what their return value is
-    #       by examining the main-function carefully.
-    #
-    #       You also need to consider which attributes the class needs.
-    #
-    #       You are allowed to modify the main function, but your
-    #       methods have to stay compatible with the original
-    #       since the automatic tests assume that.
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
+        # sale percentage is always 0.0% in new objects
+        self.sale_percentage = 0.0
+
+    def printout(self):
+        print(self.name)
+        print(f"  price: {self.price:.2f}")
+        print(f"  sale%: {self.sale_percentage:.2f}")
+
+    def set_sale_percentage(self, percentage: float):
+        if percentage >= 0 or percentage <= 100:
+            self.sale_percentage = percentage
+
+    def get_price(self) -> float:
+        return self.price * (1 - self.sale_percentage / 100)
 
 
 def main():
-    ################################################################
-    #                                                              #
-    #  You can use the main-function to test your Product class.   #
-    #  The automatic tests will not use the main you submitted.    #
-    #                                                              #
-    #  Voit käyttää main-funktiota Product-luokkasi testaamiseen.  #
-    #  Automaattiset testit eivät käytä palauttamaasi mainia.      #
-    #                                                              #
-    ################################################################
 
     test_products = {
         "milk":   1.00,
